84d24ecf4ed1e7f55b20ee818126ef3ea12087b1
BerkannY <berkanny981@gmail.com>
2024-05-27 15:52:56 +0300

    <

---
+++ app/src/main/java/DiziIcindeAramaYapmak.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class DiziIcindeAramaYapmak {
+    public static void main(String[] args) {
+        int[] notlar={48,55,70,89,20,90,100};
+        int aranan = Arrays.binarySearch(notlar,55);
+        if (aranan<0){
+            System.out.println("Değer dizinin içerisinide değildir");
+        }
+        else {
+            System.out.println("Aranan değer "+aranan+" . indeksinde yer almaktadır");
+        }
+    }
+}
