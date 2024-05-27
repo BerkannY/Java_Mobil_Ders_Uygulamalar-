bdd359cc3144703659715e1363a5871ea45af90c
BerkannY <berkanny981@gmail.com>
2024-05-27 13:39:56 +0300

    <

---
+++ app/src/main/java/Diziler.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Diziler {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int[] ogrenciNo = new int[10];
+        String[] ogrenciAdi=new String[10];
+        for (int i=0; i<10;i++)
+        {
+            System.out.println(i+". Öğrenci Numarası Girin");
+            ogrenciNo[i]=input.nextInt();
+            System.out.println(i+". Öğrenci adını giriniz");
+            ogrenciAdi[i]=input.next();
+        }
+
+       }
+
+
+    }
+
+
